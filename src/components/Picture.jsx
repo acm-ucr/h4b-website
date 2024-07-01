ba0e83aeb85874fa3cf209310f1add5ab1db16b7
Jefren Ofiaza <jefrenofiaza@gmail.com>
2024-07-01 13:45:14 -0700

    made picture component

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -1,5 +1,15 @@
-const Picture = () => {
-  return <div>Picture</div>;
+import Image from "/public/example.webp";
+import React from "react";
+
+const Picture = ({ x, y, image }) => {
+  return (
+    <div className="w-fit h-fit">
+      <Image
+        style={{ boxShadow: `${x}px ${y}px 0px 0px background: #7B3F00` }}
+        src={image}
+      />
+    </div>
+  );
 };
 
 export default Picture;
