5371ffa8e85d1dcdbb81551bf3bdb10b00684d54
qhgill <qhgill2@gmail.com>
2024-07-02 12:27:26 -0700

    added involve text and poitioning

--- src/components/home/Involve.jsx
+++ src/components/home/Involve.jsx
@@ -1,5 +1,19 @@
+import Title from "../Title";
+
 const Involve = () => {
-  return <div>Involve</div>;
+  return (
+    <div className="flex flex-col justify-center text-center">
+      <Title />
+      <div className="flex font-omc font-light justify-center text-center text-2xl">
+        Our food & information tabling occurs every <br />
+        Wednesday from 9 AM to 3 PM around the Bell Tower <br />
+        All that is needed to participate is fill the <br />
+        Food Tabling Sign-Up Sheet to choose your <br />
+        preferred time slot <br />
+      </div>
+      <p>button placeholder</p>
+    </div>
+  );
 };
 
 export default Involve;
