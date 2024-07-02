5a23bbc87c7f0f7d928ef0814f90915d1adef09a
Austin Le <ale193@ucr.edu>
2024-07-02 15:40:42 -0700

    Implemented board member card

--- src/app/board/page.js
+++ src/app/board/page.js
@@ -1,5 +1,17 @@
+import Board from "@/components/board/Board";
+import example from "@/public/example.webp";
 const Page = () => {
-  return <>Board</>;
+  return (
+    <div className="justify-center items-center flex flex-col h-screen">
+      <Board
+        name="Hana Guruz"
+        role="President"
+        yearMajor=" 3rd Year Cell, Molecular, and Developmental Biology"
+        funFact="Fun Fact: I almost became a polo player"
+        picture={example}
+      />
+    </div>
+  );
 };
 
 export default Page;
--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -1,3 +1,56 @@
-const Board = () => {};
+import Image from "next/image";
+const Board = ({ name, role, yearMajor, funFact, picture }) => {
+  return (
+    <div className="container w-60 h-90 bg-biscuits-yellow shadow-[20px_-15px_0px_rgb(190,156,106)]">
+      <div className="justify-center">
+        <Image
+          src={picture}
+          alt="picture"
+          className="aspect-square object-scale-down object-contain h-40 w-64"
+          width={100}
+          height={100}
+        />
+
+        <div className="text-wrap text-center">
+          <p
+            className={
+              "text-biscuits-green-200 font-shrikhand text-xl p-1 -translate-y-1"
+            }
+          >
+            {name}
+          </p>
+
+          <div>
+            <p
+              className={"text-biscuits-green-200 font-omc p-1 -translate-y-3"}
+            >
+              {role}
+            </p>
+
+            <div>
+              <p
+                className={
+                  "text-biscuits-gray-200 font-inika p-1 -translate-y-3 font-bold"
+                }
+              >
+                {yearMajor}
+              </p>
+
+              <div>
+                <p
+                  className={
+                    "text-biscuits-gray-200 font-inika p-1 -translate-y-3 font-bold"
+                  }
+                >
+                  {funFact}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Board;
