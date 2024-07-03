15ce07f9ae84056169064b97c48f8df6720efe51
Austin Le <ale193@ucr.edu>
2024-07-03 15:28:18 -0700

    Fixed board issue size and removed desktop file from branch

--- src/app/board/page.js
+++ src/app/board/page.js
@@ -1,17 +1,5 @@
-import Board from "@/components/board/Board";
-import example from "@/public/example.webp";
 const Page = () => {
-  return (
-    <div className="justify-center items-center flex flex-col h-screen">
-      <Board
-        name="Hana Guruz"
-        role="President"
-        yearMajor=" 3rd Year Cell, Molecular, and Developmental Biology"
-        funFact="Fun Fact: I almost became a polo player"
-        picture={example}
-      />
-    </div>
-  );
+  return <div>Board</div>;
 };
 
 export default Page;
--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -1,49 +1,24 @@
 import Image from "next/image";
 const Board = ({ name, role, yearMajor, funFact, picture }) => {
   return (
-    <div className="container w-60 h-90 bg-biscuits-yellow shadow-[20px_-15px_0px_rgb(190,156,106)]">
-      <div className="justify-center">
+    <div className="flex justify-center w-64 h-96 bg-biscuits-yellow shadow-[20px_-15px_0px_rgb(190,156,106)]">
+      <div className="">
         <Image
           src={picture}
           alt="picture"
-          className="aspect-square object-scale-down object-contain h-40 w-64"
-          width={100}
+          className="aspect-square object-center object-scale-down my-4 mx-10"
+          width={180}
           height={100}
         />
 
-        <div className="text-wrap text-center">
-          <p
-            className={
-              "text-biscuits-green-200 font-shrikhand text-xl p-1 -translate-y-1"
-            }
-          >
-            {name}
-          </p>
-
-          <div>
-            <p
-              className={"text-biscuits-green-200 font-omc p-1 -translate-y-3"}
-            >
-              {role}
-            </p>
-
-            <div>
-              <p
-                className={
-                  "text-biscuits-gray-200 font-inika p-1 -translate-y-3 font-bold"
-                }
-              >
-                {yearMajor}
-              </p>
-
-              <div>
-                <p
-                  className={
-                    "text-biscuits-gray-200 font-inika p-1 -translate-y-3 font-bold"
-                  }
-                >
-                  {funFact}
-                </p>
+        <div className="text-wrap text-center text-biscuits-green-200 font-shrikhand p-1 -translate-y-1 text-3xl ">
+          {name}
+          <div className="text-biscuits-green-200 font-omc p-1 -translate-y-2 text-xl">
+            {role}
+            <div className="text-biscuits-gray-200 font-inika p-1 -translate-y-2 font-bold text-base">
+              {yearMajor}
+              <div className="text-biscuits-gray-200 font-inika p-1 -translate-y-2 font-bold text-base">
+                {funFact}
               </div>
             </div>
           </div>
