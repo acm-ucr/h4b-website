00ada6f87faeae4e7c27fef291bc655a0388e724
Sean Quiambao <slquiambao03@gmail.com>
2024-07-03 15:47:38 -0700

    Update src/components/Picture.jsx

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -4,7 +4,7 @@ const Picture = ({ x, y, image }) => {
   return (
     <div className="w-fit h-fit">
       <Image
-        style={{ boxShadow: `${x}px ${y}px 0px 0px background: #7B3F00` }}
+        style={{ boxShadow: `${x}px ${y}px 0px 0px #7B3F00` }}
         src={image}
       />
     </div>
