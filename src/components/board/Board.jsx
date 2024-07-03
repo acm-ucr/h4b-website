559dd6e76ec9c4b94710794e96fdf3168a4e021c
Austin Le <ale193@ucr.edu>
2024-07-03 15:48:57 -0700

    Fixed board file formatting

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 const Board = ({ name, role, yearMajor, funFact, picture }) => {
   return (
     <div className="flex justify-center w-64 h-96 bg-biscuits-yellow shadow-[20px_-15px_0px_rgb(190,156,106)]">
-      <div className="">
+      <div>
         <Image
           src={picture}
           alt="picture"
@@ -13,15 +13,18 @@ const Board = ({ name, role, yearMajor, funFact, picture }) => {
 
         <div className="text-wrap text-center text-biscuits-green-200 font-shrikhand p-1 -translate-y-1 text-3xl ">
           {name}
-          <div className="text-biscuits-green-200 font-omc p-1 -translate-y-2 text-xl">
-            {role}
-            <div className="text-biscuits-gray-200 font-inika p-1 -translate-y-2 font-bold text-base">
-              {yearMajor}
-              <div className="text-biscuits-gray-200 font-inika p-1 -translate-y-2 font-bold text-base">
-                {funFact}
-              </div>
-            </div>
-          </div>
+        </div>
+
+        <div className="text-biscuits-green-200 font-omc p-1 -translate-y-2 text-xl">
+          {role}
+        </div>
+
+        <div className="text-biscuits-gray-200 font-inika p-1 -translate-y-2 font-bold text-base">
+          {yearMajor}
+        </div>
+
+        <div className="text-biscuits-gray-200 font-inika p-1 -translate-y-2 font-bold text-base">
+          {funFact}
         </div>
       </div>
     </div>
