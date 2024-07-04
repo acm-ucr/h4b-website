27e4e21ebca920c6fa30ad3e7a8a75baa1b7842d
HaokunYang123 <t300025hao@gmail.com>
2024-07-04 16:46:37 -0700

    Implemented AdditionalInfo

--- src/components/involvement/AdditionalInfo.jsx
+++ src/components/involvement/AdditionalInfo.jsx
@@ -4,13 +4,13 @@ const AdditionalInfo = () => {
   const name = "Info";
   return (
     <div className="bg-biscuits-peach p-8 text-center ">
-      <h1 className="text-8xl font-normal text-biscuits-brown-200 mb-20 font-shrikhand left-80">
+      <div className="text-8xl font-normal text-biscuits-brown-200 mb-20 font-shrikhand">
         Additional {name}
-      </h1>
-      <p className="text-4xl text-center mb-12 font-inika font-normal leading-10">
+      </div>
+      <div className="text-2xl text-center mb-12 font-inika font-normal leading-10">
         Since a H4B board member will accompany you, we kindly ask for no more
         than one person per time slot.
-      </p>
+      </div>
       <a
         href="https://docs.google.com/forms/d/e/1FAIpQLSduexmqB7bXaydEKOG5h7GjIwLGRVT0FdIqyYBE4WzyNO2k2g/viewform"
         target="_blank"
@@ -19,9 +19,9 @@ const AdditionalInfo = () => {
           SIGN UP
         </button>
       </a>
-      <p className="text-4xl flex justify-center font-inika font-normal leading-10 mb-12">
+      <div className="text-2xl flex justify-center font-inika font-normal leading-10 mb-12">
         Here are some talking points while tabling
-      </p>
+      </div>
       <a
         href="https://docs.google.com/document/d/1tp1QWfc2W1Jh8vkc32TeL17PM5dYtjgMGbx7VrPZpkY/edit?usp=sharing"
         target="_blank"
