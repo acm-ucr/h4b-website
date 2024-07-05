d9427319c2ac99ac804ff05abe13b958bf64ba15
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2024-07-05 10:40:42 -0700

    week 2 demo

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,19 @@
 import Demo from "@/components/Demo";
+import { STUDENTS } from "@/data/Student";
 
 const Home = () => {
   return (
-    <div className="flex flex-col h-screen justify-center items-center font-shrikhand bg-biscuits-red-100 text-5xl">
-      <Demo text="Hello World" color="text-white" />
+    <div className="bg-biscuits-red-100 w-screen h-fit">
+      <div className=" grid grid-cols-3 w-3/4">
+        {STUDENTS.map((student, index) => (
+          <Demo
+            key={index}
+            name={student.name}
+            major={student.major}
+            pic={student.img}
+          />
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,5 +1,13 @@
-const Demo = ({ text, color }) => {
-  return <div className={`${color}`}>{text}</div>;
+import Image from "next/image";
+
+const Demo = ({ name, major, pic }) => {
+  return (
+    <div className="bg-white w-1/3 h-fit">
+      <Image src={pic} alt={name} width={200} height={200} />
+      <div>{name}</div>
+      <div>{major}</div>
+    </div>
+  );
 };
 
 export default Demo;
