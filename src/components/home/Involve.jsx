647c3542b8e9fc46452cfd1dcae07a1942c656da
qhgill <qhgill2@gmail.com>
2024-07-04 19:15:12 -0700

    fixed br tags

--- src/components/home/Involve.jsx
+++ src/components/home/Involve.jsx
@@ -5,12 +5,10 @@ const Involve = () => {
   return (
     <div className="flex flex-col justify-center items-center text-center">
       <Title text="Get Involved" />
-      <div className="flex font-light justify-center text-center text-2xl pb-4">
-        Our food & information tabling occurs every <br />
-        Wednesday from 9 AM to 3 PM around the Bell Tower <br />
-        All that is needed to participate is fill the <br />
-        Food Tabling Sign-Up Sheet to choose your <br />
-        preferred time slot <br />
+      <div className="flex font-light justify-center text-center text-2xl pb-4 w-1/2">
+        Our food & information tabling occurs every Wednesday from 9 AM to 3 PM
+        around the Bell Tower All that is needed to participate is fill the Food
+        Tabling Sign-Up Sheet to choose your preferred time slot
       </div>
       <Button text="SIGN UP" />
     </div>
