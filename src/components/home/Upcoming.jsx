f6dad7626d6dcf43ff8795842e13f2abe31d3a05
xwRongx <57518285+xwRongx@users.noreply.github.com>
2024-07-05 09:37:47 -0700

    requested changes, trouble with padding

--- src/components/home/Upcoming.jsx
+++ src/components/home/Upcoming.jsx
@@ -5,10 +5,10 @@ const Upcoming = () => {
     <div>
       Upcoming
       <div className="m-11 max-w-4xl grid grid-cols-2 gap-4 ">
-        <UpcomingCard></UpcomingCard>
-        <UpcomingCard></UpcomingCard>
-        <UpcomingCard></UpcomingCard>
-        <UpcomingCard></UpcomingCard>
+        <UpcomingCard />
+        <UpcomingCard />
+        <UpcomingCard />
+        <UpcomingCard />
       </div>
     </div>
   );
--- src/components/home/UpcomingCard.jsx
+++ src/components/home/UpcomingCard.jsx
@@ -1,13 +1,20 @@
-const UpcomingCard = ({ month, date, time, event, location, description }) => {
+const UpcomingCard = ({
+  month = "FEB",
+  date = "19",
+  time = "8PM",
+  event = "Tabling Event",
+  location = "Location",
+  description = "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
+}) => {
   return (
     <div>
-      <div className="grid grid-cols-3 gap-x-2 border-2 border-biscuits-purple-200  rounded-3xl">
+      <div className="grid grid-cols-3 gap-x-2 border-2 border-biscuits-purple-200 py-3 rounded-3xl">
         <div className="grid bg-biscuits-purple-100 items-center justify-center rounded-l-3xl text-white">
           <p className="text-3xl text-center font-omc">{month}</p>
           <p className="text-5xl text-center font-omc">{date}</p>
           <p className="text-lg text-center font-omc">{time}</p>
         </div>
-        <div className="col-span-2">
+        <div className="bg-white rounded-r-3xl col-span-2">
           <p className="text-2xl font-omc">{event}</p>
           <p className="text-m font-omc">{location}</p>
           <p className="text-sm ">{description}</p>
@@ -15,16 +22,6 @@ const UpcomingCard = ({ month, date, time, event, location, description }) => {
       </div>
     </div>
   );
-};
-
-UpcomingCard.defaultProps = {
-  month: "FEB",
-  date: "19",
-  time: "8PM",
-  event: "Tabling Event",
-  location: "Location",
-  description:
-    "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
 };
 
 export default UpcomingCard;
