65c0100fcb46524ce184977720ab72cfa107cc32
qhgill <qhgill2@gmail.com>
2024-07-04 19:09:04 -0700

    finished involve and button adjustments

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ const Button = ({ text }) => {
     <>
       <Link
         href="https://example.com/"
-        className="inline-block bg-biscuits-pinkish text-biscuits-textColor font-inika py-2.4 px-8 rounded-full font-bold text-lg duration-300 hover:opacity-80"
+        className="w-fit bg-biscuits-pinkish text-biscuits-textColor font-inika py-2.4 px-8 rounded-full font-bold text-2xl duration-300 hover:opacity-80"
       >
         {text}
       </Link>
--- src/components/home/Involve.jsx
+++ src/components/home/Involve.jsx
@@ -1,17 +1,18 @@
 import Title from "../Title";
+import Button from "../Button";
 
 const Involve = () => {
   return (
-    <div className="flex flex-col justify-center text-center">
+    <div className="flex flex-col justify-center items-center text-center">
       <Title text="Get Involved" />
-      <div className="flex font-omc font-light justify-center text-center text-4xl">
+      <div className="flex font-light justify-center text-center text-2xl pb-4">
         Our food & information tabling occurs every <br />
         Wednesday from 9 AM to 3 PM around the Bell Tower <br />
         All that is needed to participate is fill the <br />
         Food Tabling Sign-Up Sheet to choose your <br />
         preferred time slot <br />
       </div>
-      <p>button placeholder</p>
+      <Button text="SIGN UP" />
     </div>
   );
 };
