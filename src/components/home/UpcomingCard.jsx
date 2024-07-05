82438b8d27557a697af4312698c8eb2e71848410
xwRongx <57518285+xwRongx@users.noreply.github.com>
2024-07-04 19:15:53 -0700

    implemented upcomingCard component

--- src/components/home/UpcomingCard.jsx
+++ src/components/home/UpcomingCard.jsx
@@ -1,3 +1,30 @@
-const UpcomingCard = () => {};
+const UpcomingCard = ({ month, date, time, event, location, description }) => {
+  return (
+    <div>
+      <div className="grid grid-cols-3 gap-x-2 border-2 border-biscuits-purple-200  rounded-3xl">
+        <div className="grid bg-biscuits-purple-100 items-center justify-center rounded-l-3xl text-white">
+          <p className="text-3xl text-center font-omc">{month}</p>
+          <p className="text-5xl text-center font-omc">{date}</p>
+          <p className="text-lg text-center font-omc">{time}</p>
+        </div>
+        <div className="col-span-2">
+          <p className="text-2xl font-omc">{event}</p>
+          <p className="text-m font-omc">{location}</p>
+          <p className="text-sm ">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+UpcomingCard.defaultProps = {
+  month: "FEB",
+  date: "19",
+  time: "8PM",
+  event: "Tabling Event",
+  location: "Location",
+  description:
+    "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
+};
 
 export default UpcomingCard;
