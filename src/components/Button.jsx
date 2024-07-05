f97442674380d357f6f257b5ffeca3cc61b3bf80
Russell Villanueva <russellv.git@gmail.com>
2024-07-04 18:07:14 -0700

    Added matching colors according to the Figma

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ const Button = ({ text }) => {
     <>
       <Link
         href="https://example.com/"
-        className="inline-block bg-biscuits-purple-100 text-black font-inika py-2.4 px-8 rounded-full font-bold text-lg duration-300 hover:opacity-80"
+        className="inline-block bg-biscuits-pinkish text-biscuits-textColor font-inika py-2.4 px-8 rounded-full font-bold text-lg duration-300 hover:opacity-80"
       >
         {text}
       </Link>
