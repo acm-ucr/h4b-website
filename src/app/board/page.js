6d9f5fab5b87b155b5b6a76cbb5c2fd29da15e9e
xwRongx <57518285+xwRongx@users.noreply.github.com>
2024-07-08 03:02:50 -0700

    implement board page

--- src/app/board/page.js
+++ src/app/board/page.js
@@ -1,5 +1,27 @@
+import Board from "@/components/board/Board";
+import { BOARD } from "@/data/boardMembers";
+import Title from "@/components/Title";
+
 const Page = () => {
-  return <div>Board</div>;
+  return (
+    <div>
+      <div>
+        <Title text="Get To Know Us" />
+        <div className=" grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-y-16 gap-x-20 mx-16">
+          {BOARD.map((boardMember, index) => (
+            <Board
+              key={index}
+              name={boardMember.name}
+              role={boardMember.role}
+              yearMajor={boardMember.yearMajor}
+              funFact={boardMember.funFact}
+              picture={boardMember.picture}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
