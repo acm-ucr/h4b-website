566fd3fd9120c59c61b13924e0b8129f61941b33
twu148 <twu148@ucr.edu>
2024-07-08 15:16:47 +0900

    Small fixes to fix screen width errors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="flex w-screen items-center p-2 mt-[10%] bg-biscuits-peach justify-between text-biscuits-purple-200 font-omc text-3xl">
+    <div className="flex  items-center p-2 mt-[10%] bg-biscuits-peach justify-between text-biscuits-purple-200 font-omc text-3xl">
       <div className="flex tracking-widest"> H4B at UCR </div>
       <div className="flex flex-row space-x-2">
         {footer.map((social, index) => (
