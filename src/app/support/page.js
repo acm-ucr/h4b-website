c401fab64bca69ff0dc732776a03a87c5e312a04
Austin Le <ale193@ucr.edu>
2024-07-08 14:44:00 -0700

    Updating connect and support page with styling changes

--- src/app/support/page.js
+++ src/app/support/page.js
@@ -1,22 +1,8 @@
-import Connect from "@/components/support/Connect";
-import { SOCIALS } from "@/data/support/Socials";
-
+import Support from "@/components/support/Support";
 const Page = () => {
   return (
     <>
-      <div className="text-center text-biscuits-brown-200 font-shrikhand p-1 -translate-y-1 text-3xl">
-        {"Connect with us!"}
-        <div className="flex space-x-6 justify-center">
-          {SOCIALS.map((socials, index) => (
-            <Connect
-              key={index}
-              name={socials.name}
-              icon={socials.icon}
-              link={socials.link}
-            />
-          ))}
-        </div>
-      </div>
+      <Support />
     </>
   );
 };
--- src/components/support/Connect.jsx
+++ src/components/support/Connect.jsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 const Connect = ({ name, icon, link }) => {
   return (
-    <div className="text-6xl text-biscuits-purple-200">
+    <div className="text-8xl text-biscuits-purple-200">
       <Link href={link}>{icon}</Link>
-      <div className="text-xs">{name}</div>
+      <div className="text-xl">{name}</div>
     </div>
   );
 };
