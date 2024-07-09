7e56cf41768a40646d21dc25441b1970db81f756
Sean Quiambao <slquiambao03@gmail.com>
2024-07-08 23:00:37 -0700

    Update src/components/Subtitle.jsx

--- src/components/Subtitle.jsx
+++ src/components/Subtitle.jsx
@@ -1,6 +1,6 @@
 const Subtitle = ({ color, text }) => {
   return (
-    <div className={`font-bold font-Shrikhand text-7xl ${color}`}>{text}</div>
+    <div className={`font-bold font-shrikhand text-7xl ${color}`}>{text}</div>
   );
 };
 
