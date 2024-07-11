45c5048412ca6a6031cbd35424ba7559f40b26dd
Jefren Ofiaza <jefrenofiaza@gmail.com>
2024-07-10 18:35:01 -0700

    added title.js

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,12 +1,31 @@
-const Title = ({ text }) => {
+import { COOKIES, COLORS } from "@/data/Title";
+
+const Title = ({ text, food, color }) => {
+  let lightColor = "biscuits-gray-100";
+  let darkColor = "biscuits-gray-200";
+
+  if (color && COLORS[color]) {
+    lightColor = COLORS[color].light;
+    darkColor = COLORS[color].dark;
+  }
+
   return (
     <div className="inset-0 flex justify-center items-top p-3">
-      <div className="absolute font-omc text-biscuits-gray-200 text-8xl mb-4">
+      <div
+        className="absolute font-omc text-8xl mb-4"
+        style={{ color: lightColor }}
+      >
         {text}
       </div>
-      <div className="relative font-omc text-biscuits-gray-100 text-8xl mb-2 drop-shadow left-1 bottom-0.5">
+      <div
+        className="relative font-omc text-8xl mb-2 drop-shadow left-1 bottom-0.5"
+        style={{ color: darkColor }}
+      >
         {text}
       </div>
+      {food && COOKIES[food] && (
+        <img src={COOKIES[food]} alt={`${food} cookies`} className="ml-4" />
+      )}
     </div>
   );
 };
---
+++ src/data/Title.js
@@ -0,0 +1,21 @@
+import chipCookie from "@/images/chip.webp";
+import heartCookie from "@/images/heart.webp";
+import swirlCookie from "@images/heart.webp";
+
+export const COOKIES = {
+  chip: chipCookie,
+  heart: heartCookie,
+  swirl: swirlCookie,
+};
+
+export const COLORS = {
+  red: {
+    light: "#DC8686",
+    dark: "#8C4E4E",
+  },
+
+  blue: {
+    light: "#9BB0C1",
+    dark: "2C4755",
+  },
+};
