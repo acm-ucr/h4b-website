f95505cb142198316cefe6e2f54d6f443ebd4997
Jefren Ofiaza <jefrenofiaza@gmail.com>
2024-07-10 20:48:31 -0700

    hopefully last changes

--- src/data/Title.js
+++ src/data/Title.js
@@ -1,6 +1,6 @@
-import chipCookie from "@/images/chip.webp";
-import heartCookie from "@/images/heart.webp";
-import swirlCookie from "@/images/swirl.webp";
+import chipCookie from "@/public/images/chip.webp";
+import heartCookie from "@/public/images/heart.webp";
+import swirlCookie from "@/public/images/swirl.webp";
 
 export const COOKIES = {
   chip: chipCookie,
@@ -10,12 +10,12 @@ export const COOKIES = {
 
 export const COLORS = {
   red: {
-    light: "text-red-100",
-    dark: "text-red-200",
+    light: "text-biscuits-red-100",
+    dark: "text-biscuits-red-200",
   },
 
-  blue: {
-    light: "text-blue-100",
-    dark: "text-blue-200",
+  purple: {
+    light: "text-biscuits-purple-100",
+    dark: "text-biscuits-purple-200",
   },
 };
