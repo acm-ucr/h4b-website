9c3881640d3b2f8e2e21f0ef08a2c6996a7cd328
Nimrah Saleem <nimsaleem44@gmail.com>
2024-07-12 09:35:58 -0700

    need help with picture format

--- src/components/involvement/Tabling.jsx
+++ src/components/involvement/Tabling.jsx
@@ -1,5 +1,24 @@
+import { TABLING } from "@/data/involve/Tabling";
+import Picture from "../Picture";
+import example from "@/public/images/example.webp";
+
 const Tabling = () => {
-  return <div>Tabling</div>;
+  return (
+    <div className="flex w-screen flex-col">
+      <p className="text-biscuits-brown-200 font-normal font-shrikhand text-6xl">
+        How to Table
+      </p>
+
+      <Picture x={20} y={20} image={example} className="float-right" />
+
+      {TABLING.map((tabling, index) => (
+        <div key={index} className="flex flex-row font-inika text-2xl">
+          <p className="font-bold">{tabling.number} </p>
+          <p>{tabling.bullet}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Tabling;
