636342e9b86f7d8af37c35892dfb5144338ea3b4
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2024-07-12 11:08:48 -0700

    Nsaleem1/Tabling

--- src/components/involvement/Tabling.jsx
+++ src/components/involvement/Tabling.jsx
@@ -1,13 +1,12 @@
 import { TABLING } from "@/data/involve/Tabling";
 import Picture from "../Picture";
 import example from "@/public/images/example.webp";
+import Subtitle from "../Subtitle";
 
 const Tabling = () => {
   return (
     <div className="flex w-3/4 flex-col items-center gap-y-8">
-      <p className="text-biscuits-brown-200 font-normal font-shrikhand text-6xl">
-        How to Table
-      </p>
+      <Subtitle color="text-biscuits-brown-200" text="How to Table" />
 
       <Picture x={20} y={20} image={example} className="px-4 my-4" />
 
