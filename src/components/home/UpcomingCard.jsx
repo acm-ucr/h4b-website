3c165a3bafc279a273f6f2b0211aa171499f8ff8
twu148 <twu148@ucr.edu>
2024-07-19 12:35:06 -0700

    Added mobile responsiveness for Upcoming Events

--- src/components/home/UpcomingCard.jsx
+++ src/components/home/UpcomingCard.jsx
@@ -10,14 +10,20 @@ const UpcomingCard = ({
     <div>
       <div className="flex flex-row rounded-3xl">
         <div className="bg-biscuits-purple-100 items-center rounded-l-3xl justify-center text-white text-shadow shadow-biscuits-purple-200 py-3 w-1/3">
-          <div className="text-5xl text-center font-omc">{month}</div>
-          <div className="text-7xl text-center font-omc">{date}</div>
-          <div className="text-4xl text-center font-omc">{time}</div>
+          <div className="text-3xl md:text-5xl text-center font-omc">
+            {month}
+          </div>
+          <div className="text-5xl md:text-7xl text-center font-omc">
+            {date}
+          </div>
+          <div className="text-2xl md:text-4xl text-center font-omc">
+            {time}
+          </div>
         </div>
         <div className="flex flex-col bg-white w-3/4 rounded-r-3xl text-left pl-5 justify-center">
-          <div className="text-5xl font-omc">{event}</div>
-          <div className="text-m font-omc">{location}</div>
-          <div className="text-sm ">{description}</div>
+          <div className="text-3xl md:text-5xl font-omc">{event}</div>
+          <div className="text-sm md:text-m font-omc">{location}</div>
+          <div className="text-xs md:text-sm ">{description}</div>
         </div>
       </div>
     </div>
