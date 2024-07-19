d09f63766861f3433e1533546fff95686e5c616f
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2024-07-18 21:41:42 -0700

    forgot to push other files

--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -3,10 +3,8 @@ import landing from "@/public/images/landing.webp";
 
 const Landing = () => {
   return (
-    <div className="m-10">
-      <div className="p-6 pb-10  border-8 flex justify-center items-center border-biscuits-orange-200 rounded-3xl bg-biscuits-peach-200 bg-opacity-60">
-        <Image src={landing} alt="landing" />
-      </div>
+    <div className="p-6 pb-10  border-8 flex justify-center items-center border-biscuits-orange-200 rounded-3xl bg-biscuits-peach-200 bg-opacity-60 w-3/4">
+      <Image src={landing} alt="landing" width={1250} height={1250} />
     </div>
   );
 };
