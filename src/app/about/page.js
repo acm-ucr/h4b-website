9b639b766c3c76dd653e2459c6117731e32f2708
Jefren Ofiaza <jefrenofiaza@gmail.com>
2024-07-19 10:49:25 -0700

    use subtitle remove comments

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,8 @@
-import springPicture from "@/public/images/spring/spring24_9.webp"; 
+import springPicture from "@/public/images/spring/spring24_9.webp";
 import { ABOUT } from "@/data/aboutPage";
 import Title from "@/components/Title";
 import Picture from "@/components/Picture";
+import Subtitle from "@/components/Subtitle";
 
 const Page = () => {
   return (
@@ -16,9 +17,7 @@ const Page = () => {
       />
       {ABOUT.map((section, index) => (
         <div key={index} className="mb-8 text-center">
-          <div className="italic text-biscuits-green-200 font-shrikhand text-4xl mb-4">
-            {section.subtitle}
-          </div>
+          <Subtitle color="text-biscuits-green-200" text={section.subtitle} />
           <div className="text-2xl font-inika w-2/3 mx-auto">
             {section.paragraph}
           </div>
