bef379c8731492cf159889f74001ddc39d48f138
Austin Le <ale193@ucr.edu>
2024-07-19 15:46:57 -0700

    Implemented the pics into the board page

--- src/data/board/Board.js
+++ src/data/board/Board.js
@@ -1,4 +1,10 @@
-import examplePic from "@/public/images/example.webp";
+import image1 from "@/public/images/board/hana.webp";
+import image2 from "@/public/images/board/alyanna.webp";
+import image3 from "@/public/images/board/mylinh.webp";
+import image4 from "@/public/images/board/kelly.webp";
+import image5 from "@/public/images/board/zina.webp";
+import image6 from "@/public/images/board/sansita.webp";
+import image7 from "@/public/images/board/kaylee.webp";
 
 export const BOARD = [
   {
@@ -6,48 +12,48 @@ export const BOARD = [
     role: "President",
     yearMajor: "3rd Year Cell, Molecular, and Developmental Biology",
     funFact: "Fun Fact: I almost became a polo player",
-    picture: examplePic,
+    picture: image1,
   },
   {
     name: "Alyanna Almario",
     role: "Vice President",
     yearMajor: "3rd Year Bioengineering",
     funFact: "Fun Fact: I stream on Twitch",
-    picture: examplePic,
+    picture: image2,
   },
   {
     name: "My Linh Le",
     role: "External Relations",
     yearMajor: "3rd Year Bioengineering",
     funFact: "Fun Fact: I like to draw and paint on my free time",
-    picture: examplePic,
+    picture: image3,
   },
   {
     name: "Kelly Tang",
     role: "Team Leader",
     yearMajor: "3rd Year Business Administration",
     funFact: "Fun Fact: I'm allergic to cat fur",
-    picture: examplePic,
+    picture: image4,
   },
   {
     name: "Zina Vo",
     role: "Secretary",
     yearMajor: "2nd Year Business Administration",
     funFact: "Fun Fact: I almost got dragged by a cow",
-    picture: examplePic,
+    picture: image5,
   },
   {
     name: "Sansita Narra",
     role: "Treasurer",
     yearMajor: "3rd Year Biology",
     funFact: "Fun Fact: I've lived in two different countries before",
-    picture: examplePic,
+    picture: image6,
   },
   {
     name: "Kaylee Oka",
     role: "Social Media Manager",
     yearMajor: "2nd Year Neuroscience & Psychology",
     funFact: "Fun Fact: I have two dogs",
-    picture: examplePic,
+    picture: image7,
   },
 ];
