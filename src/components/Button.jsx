94be36d0f802fca26048f3b65f1709f08514219a
Fuyuki Malahom <fuyuki325@gmail.com>
2024-07-23 08:02:17 -0700

    button bigger

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,7 +6,7 @@ const Button = ({ text, link }) => {
       <Link
         href={link}
         target="_blank"
-        className="w-fit bg-biscuits-pinkish text-biscuits-textColor font-inika py-2.4 px-8 rounded-full font-bold text-2xl duration-300 hover:opacity-80"
+        className="w-fit bg-biscuits-pinkish text-biscuits-textColor font-inika py-2 px-10 md:py-3 md:px-12 rounded-full font-bold text-2xl duration-300 hover:opacity-80"
       >
         {text}
       </Link>
