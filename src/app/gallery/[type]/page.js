6ebf2b57116ad0192897c4c62dbebd75138b4cbd
qhgill <qhgill2@gmail.com>
2024-07-23 17:44:35 -0700

    gallery setup

--- src/app/gallery/[type]/page.js
+++ src/app/gallery/[type]/page.js
@@ -1,5 +1,7 @@
 import NotFound from "@/app/not-found";
 import Title from "@/components/Title";
+import Picture from "@/components/Picture.jsx";
+import { PICTURES } from "@/data/gallery/pictures.js";
 
 export async function generateStaticParams() {
   const allPages = ["fall", "winter", "spring", "past"];
@@ -17,6 +19,13 @@ const Page = ({ params }) => {
     return (
       <div className="text-7xl">
         <Title text={PAGES[params.type]} food={"chip"} />
+        <div className="grid-cols-3">
+          {PICTURES[params.type].map((image, index) => (
+            <div key={index} className="">
+              <Picture x={1} y={1} src={image} alt={"img"} />
+            </div>
+          ))}
+        </div>
       </div>
     );
   } else {
---
+++ src/data/gallery/pictures.js
@@ -0,0 +1,169 @@
+import fall231 from "@/public/images/fall/fall23_1.webp";
+import fall232 from "@/public/images/fall/fall23_2.webp";
+import fall233 from "@/public/images/fall/fall23_3.webp";
+import fall234 from "@/public/images/fall/fall23_4.webp";
+import fall235 from "@/public/images/fall/fall23_5.webp";
+import fall236 from "@/public/images/fall/fall23_6.webp";
+import fall238 from "@/public/images/fall/fall23_8.webp";
+import fall239 from "@/public/images/fall/fall23_9.webp";
+import fall2310 from "@/public/images/fall/fall23_10.webp";
+import fall2311 from "@/public/images/fall/fall23_11.webp";
+import fall2312 from "@/public/images/fall/fall23_12.webp";
+import fall2313 from "@/public/images/fall/fall23_13.webp";
+import fall2314 from "@/public/images/fall/fall23_14.webp";
+import fall2315 from "@/public/images/fall/fall23_15.webp";
+import fall2316 from "@/public/images/fall/fall23_16.webp";
+import fall2317 from "@/public/images/fall/fall23_17.webp";
+import fall2318 from "@/public/images/fall/fall23_18.webp";
+import fall2319 from "@/public/images/fall/fall23_19.webp";
+import fall2320 from "@/public/images/fall/fall23_20.webp";
+import fall2321 from "@/public/images/fall/fall23_21.webp";
+import fall2322 from "@/public/images/fall/fall23_22.webp";
+import fall2323 from "@/public/images/fall/fall23_23.webp";
+import fall2324 from "@/public/images/fall/fall23_24.webp";
+import winter241 from "@/public/images/winter/winter24_1.webp";
+import winter242 from "@/public/images/winter/winter24_2.webp";
+import winter243 from "@/public/images/winter/winter24_3.webp";
+import winter244 from "@/public/images/winter/winter24_4.webp";
+import winter245 from "@/public/images/winter/winter24_5.webp";
+import winter246 from "@/public/images/winter/winter24_6.webp";
+import winter247 from "@/public/images/winter/winter24_7.webp";
+import winter248 from "@/public/images/winter/winter24_8.webp";
+import spring241 from "@/public/images/spring/spring24_1.webp";
+import spring242 from "@/public/images/spring/spring24_2.webp";
+import spring243 from "@/public/images/spring/spring24_3.webp";
+import spring244 from "@/public/images/spring/spring24_4.webp";
+import spring245 from "@/public/images/spring/spring24_5.webp";
+import spring246 from "@/public/images/spring/spring24_6.webp";
+import spring247 from "@/public/images/spring/spring24_7.webp";
+import spring248 from "@/public/images/spring/spring24_8.webp";
+import spring249 from "@/public/images/spring/spring24_9.webp";
+
+export const PICTURES = {
+  fall: [
+    {
+      image: fall231,
+    },
+    {
+      image: fall232,
+    },
+    {
+      image: fall233,
+    },
+    {
+      image: fall234,
+    },
+    {
+      image: fall235,
+    },
+    {
+      image: fall236,
+    },
+    {
+      image: fall238,
+    },
+    {
+      image: fall239,
+    },
+    {
+      image: fall2310,
+    },
+    {
+      image: fall2311,
+    },
+    {
+      image: fall2312,
+    },
+    {
+      image: fall2313,
+    },
+    {
+      image: fall2314,
+    },
+    {
+      image: fall2315,
+    },
+    {
+      image: fall2316,
+    },
+    {
+      image: fall2317,
+    },
+    {
+      image: fall2318,
+    },
+    {
+      image: fall2319,
+    },
+    {
+      image: fall2320,
+    },
+    {
+      image: fall2321,
+    },
+    {
+      image: fall23_22,
+    },
+    {
+      image: fall2323,
+    },
+    {
+      image: fall2324,
+    },
+  ],
+  winter: [
+    {
+      image: winter241,
+    },
+    {
+      image: winter242,
+    },
+    {
+      image: winter243,
+    },
+    {
+      image: winter244,
+    },
+    {
+      image: winter245,
+    },
+    {
+      image: winter246,
+    },
+    {
+      image: winter247,
+    },
+    {
+      image: winter248,
+    },
+  ],
+  spring: [
+    {
+      image: spring241,
+    },
+    {
+      image: spring242,
+    },
+    {
+      image: spring243,
+    },
+    {
+      image: spring244,
+    },
+    {
+      image: spring245,
+    },
+    {
+      image: spring246,
+    },
+    {
+      image: spring247,
+    },
+    {
+      image: spring248,
+    },
+    {
+      image: spring249,
+    },
+  ],
+};
