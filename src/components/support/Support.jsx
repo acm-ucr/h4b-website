933268499666f0413647c58b38a7ec63a70173dc
twu148 <twu148@ucr.edu>
2024-07-24 15:10:04 -0700

    Flex wrap image

--- src/components/support/Support.jsx
+++ src/components/support/Support.jsx
@@ -10,15 +10,13 @@ const Support = () => {
   return (
     <div className="px-10 bg-biscuits-orange-300">
       <Title text="Support Us" food="croissant" color="gray" />
-      <div className="">
-        <div className="md:flex justify-items-end gap-x-6 py-14 justify-evenly items-center">
-          <Image src={standing} alt="standing" className="justify-items-end" />
-          <div className="flex flex-col items-center gap-y-8 pt-12 md:pt-0">
-            <Subtitle color="text-biscuits-brown-200" text="Donations" />
-            <Button text="VENMO" link="https://account.venmo.com/u/h4b_ucr" />
-            <Subtitle color="text-biscuits-brown-200" text="Newsletter" />
-            <Button text="SIGN UP" link="http://eepurl.com/iaWAc5" />
-          </div>
+      <div className="flex flex-wrap gap-x-6 py-14 justify-evenly items-center">
+        <Image src={standing} alt="standing" className="pb-4" />
+        <div className="flex flex-col items-center gap-y-8 pt-12 md:pt-0">
+          <Subtitle color="text-biscuits-brown-200" text="Donations" />
+          <Button text="VENMO" link="https://account.venmo.com/u/h4b_ucr" />
+          <Subtitle color="text-biscuits-brown-200" text="Newsletter" />
+          <Button text="SIGN UP" link="http://eepurl.com/iaWAc5" />
         </div>
       </div>
       <div className="font-shrikhand">
@@ -28,7 +26,7 @@ const Support = () => {
         />
         <div className="flex-wrap flex justify-center md:gap-12 translate-y-5 pb-10">
           {SOCIALS.map((socials, index) => (
-            <div key={index} className="w-1/2 md:w-auto">
+            <div key={index} className="w-auto">
               <Connect
                 key={index}
                 name={socials.name}
