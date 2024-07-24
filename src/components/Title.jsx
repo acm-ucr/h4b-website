5d13d7e751b2cdcdbf9eb78204b060b498c498d5
Russell Villanueva <russellv.git@gmail.com>
2024-07-24 14:39:40 -0700

    Final Changes

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -21,12 +21,13 @@ const Title = ({ text, food, color }) => {
       </div>
 
       {food && COOKIES[food] && (
-        <div className="ml-4">
+        <div className="ml-4 ml-2 flex items-center">
           <Image
             src={COOKIES[food]}
             alt={`${food} cookies`}
             width={100}
             height={100}
+            className="w-12 h-12 md:w-24 md:h-24"
           />
         </div>
       )}
