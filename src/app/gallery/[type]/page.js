d1c0a03cc6498fdbfd34e37b799747899b6cf979
Jefren Ofiaza <jefrenofiaza@gmail.com>
2024-07-26 12:23:23 -0700

    changed sm to lg

--- src/app/gallery/[type]/page.js
+++ src/app/gallery/[type]/page.js
@@ -22,7 +22,7 @@ const Page = ({ params }) => {
     return (
       <div className="flex flex-col text-7xl items-center pt-10">
         <Title text={PAGES[params.type]} food={"chip"} />
-        <div className="grid grid-cols-1 sm:grid-cols-3 w-4/5 py-10 gap-2">
+        <div className="grid grid-cols-1 lg:grid-cols-3 w-4/5 py-10 gap-2">
           {PICTURES[params.type].map((image, index) => (
             <Image
               src={image}
