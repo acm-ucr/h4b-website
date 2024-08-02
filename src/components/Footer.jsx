f3d11ed604839bd6fea4e94b4d456d72c23bb065
xwRongx <57518285+xwRongx@users.noreply.github.com>
2024-08-02 01:30:06 -0700

    add transition to hover icons, fix drop shadow

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,16 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="flex shadow-[0_-2px_3px_0px_rgba(0,0,0,0.1)] items-center p-4 bg-biscuits-peach justify-between text-biscuits-purple-200 font-omc text-2xl md:text-3xl text-center">
+    <div className="flex drop-shadow items-center p-4 bg-biscuits-peach justify-between text-biscuits-purple-200 font-omc text-2xl md:text-3xl text-center">
       <div className="hidden md:flex tracking-widest"> H4B at UCR </div>
       <div className="flex md:hidden tracking-widest"> H4B </div>
       <div className="flex flex-row space-x-2 md:space-x-5 ">
         {FOOTER.map((social, index) => (
-          <Link className="hover:-translate-y-1" key={index} href={social.link}>
+          <Link
+            className="transition ease-in-out hover:-translate-y-1"
+            key={index}
+            href={social.link}
+          >
             {social.icon}
           </Link>
         ))}
