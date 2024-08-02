60e4d713ef431b8968513ea15105cd7da5693007
Russell Villanueva <russellv.git@gmail.com>
2024-08-01 23:28:14 -0700

    Issue fixes

--- src/components/events/CustomEvents.jsx
+++ src/components/events/CustomEvents.jsx
@@ -1,7 +1,32 @@
+import React from "react";
+
+const categoryColors = {
+  "H4B Food Tabling": "#FFDDE7", // pink
+  "H4B Events": "#FFC679 ", // orange
+  "Food Resources": "#4A90E2", // Blue
+};
+
 const CustomEvent = ({ event }) => {
+  const getColor = () => {
+    if (event.title.includes("Tabling")) {
+      return categoryColors["H4B Food Tabling"];
+    }
+    if (event.title.includes("Event")) {
+      return categoryColors["H4B Events"];
+    }
+    return categoryColors["Food Resources"];
+  };
+
+  const backgroundColor = getColor();
+
   return (
-    <div>
-      <p className="text-black">{event.title}</p>
+    <div
+      className="border-2 rounded-2xl p-4 mb-4"
+      style={{ backgroundColor, borderColor: backgroundColor }}
+    >
+      <p className="text-black truncate text-center font-shrikhand">
+        {event.title}
+      </p>
     </div>
   );
 };
--- src/components/support/Support.jsx
+++ src/components/support/Support.jsx
@@ -5,6 +5,7 @@ import { SOCIALS } from "@/data/support/Socials";
 import Title from "../Title";
 import standing from "@/public/images/standing.webp";
 import Image from "next/image";
+import Link from "next/link";
 
 const Support = () => {
   return (
@@ -13,9 +14,17 @@ const Support = () => {
       <div className="flex flex-wrap gap-x-6 py-14 justify-evenly items-center">
         <Image src={standing} alt="standing" className="pb-4" />
         <div className="flex flex-col items-center gap-y-8 pt-12 md:pt-0">
-          <Subtitle color="text-biscuits-brown-200" text="Donations" />
+          <Link href="https://account.venmo.com/u/h4b_ucr">
+            <div className="cursor-pointer text-biscuits-brown-200 hover:underline">
+              <Subtitle text="Donations" color="text-biscuits-brown-200" />
+            </div>
+          </Link>
           <Button text="VENMO" link="https://account.venmo.com/u/h4b_ucr" />
-          <Subtitle color="text-biscuits-brown-200" text="Newsletter" />
+          <Link href="http://eepurl.com/iaWAc5">
+            <div className="cursor-pointer text-biscuits-brown-200 hover:underline">
+              <Subtitle text="Newsletter" color="text-biscuits-brown-200" />
+            </div>
+          </Link>
           <Button text="SIGN UP" link="http://eepurl.com/iaWAc5" />
         </div>
       </div>
@@ -24,14 +33,13 @@ const Support = () => {
           color="text-center text-biscuits-brown-200"
           text="Connect with us!"
         />
-        <div className="mx-12 xl:mx-64 grid grid-cols-2 xl:grid-cols-4 translate-y-5 pb-10">
-          {SOCIALS.map((socials, index) => (
+        <div className="mx-12 xl:mx-64 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-x-32 gap-y-8 pb-10">
+          {SOCIALS.map((social, index) => (
             <div key={index} className="w-auto px-5">
               <Connect
-                key={index}
-                name={socials.name}
-                icon={socials.icon}
-                link={socials.link}
+                name={social.name}
+                icon={social.icon}
+                link={social.link}
               />
             </div>
           ))}
@@ -40,4 +48,5 @@ const Support = () => {
     </div>
   );
 };
+
 export default Support;
