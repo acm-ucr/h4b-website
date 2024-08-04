a26a86f94717b6e2ca7468e8c47f8fac1f98255b
twu148 <twu148@ucr.edu>
2024-08-04 11:50:58 -0700

    Moved default prop and bullet point code inside Modal, made title bigger

--- src/components/events/Modal.jsx
+++ src/components/events/Modal.jsx
@@ -1,35 +1,38 @@
 import { IoMdClose } from "react-icons/io";
 
-const Bullet = ({ item }) => (
-  <li className="flex items-center mb-1">
-    <span className="w-3 h-3 bg-biscuits-blue-100 inline-block rounded-full mr-2"></span>
-    {item}
-  </li>
-);
-
-const Modal = ({ title, date, time, place, description }) => {
+const Modal = ({
+  title = "Event Title",
+  date = "Date",
+  time = "Time",
+  place = "Place",
+  description = "No description provided.",
+}) => {
   return (
-    <div className="relative flex-col bg-opacity-55 p-6 rounded-3xl border-8 w-1/4 bg-white border-biscuits-blue-100">
+    <div className="relative flex-col bg-opacity-55 p-6 rounded-3xl border-8 w-1/3 bg-white border-biscuits-blue-100">
       <button className="absolute top-6 right-6 hover:text-biscuits-blue-100 text-2xl">
         <IoMdClose />
       </button>
-      <div className="text-xl mb-2 font-bold">{title}</div>
+      <div className="text-2xl mb-2 font-bold">{title}</div>
       <ul className="list-none">
-        <Bullet item={date} />
-        <Bullet item={time} />
-        <Bullet item={place} />
-        <Bullet item={description} />
+        <li className="flex items-center mb-1">
+          <span className="w-3 h-3 bg-biscuits-blue-100 inline-block rounded-full mr-2"></span>
+          {date}
+        </li>
+        <li className="flex items-center mb-1">
+          <span className="w-3 h-3 bg-biscuits-blue-100 inline-block rounded-full mr-2"></span>
+          {time}
+        </li>
+        <li className="flex items-center mb-1">
+          <span className="w-3 h-3 bg-biscuits-blue-100 inline-block rounded-full mr-2"></span>
+          {place}
+        </li>
+        <li className="flex items-center mb-1">
+          <span className="w-3 h-3 bg-biscuits-blue-100 inline-block rounded-full mr-2"></span>
+          {description}
+        </li>
       </ul>
     </div>
   );
-};
-
-Modal.defaultProps = {
-  title: "Event Title",
-  date: "Date",
-  time: "Time",
-  place: "Place",
-  description: "No description provided.",
 };
 
 export default Modal;
