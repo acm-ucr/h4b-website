968b9b9873a9697e573b8c6f26aeda069191c698
qhgill <qhgill2@gmail.com>
2024-08-05 18:57:32 -0700

    gcal integration initial commit

--- src/components/events/CalendarEvents.jsx
+++ src/components/events/CalendarEvents.jsx
@@ -1,32 +1,54 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import CustomEvent from "./CustomEvents";
 import CustomHeader from "./CustomHeader";
 import CustomToolbar from "./CustomToolbar";
+import Modal from "./Modal";
 
 const mLocalizer = momentLocalizer(moment);
 
 const CalendarEvents = () => {
+  const [events, setEvents] = useState([]);
   const [date, setDate] = useState(new Date());
-  const dummyEvents = [
-    {
-      title: "Tabling event 1",
-      start: new Date(2024, 7, 5, 10, 0),
-      end: new Date(2024, 7, 5, 11, 0),
-      location: "WCH",
-      summary: "the best event",
-    },
-    {
-      title: "Tabling event 2",
-      start: new Date(2024, 6, 5, 10, 0),
-      end: new Date(2024, 6, 5, 11, 0),
-      location: "WCH",
-      summary: "even better event",
-    },
-  ];
+  const [selectedEvent, setSelectedEvent] = useState(null);
+  useEffect(() => {
+    const startDate = new Date(
+      new Date().getTime() - 60 * 60 * 24 * 7 * 10 * 1000
+    ).toISOString();
+    const endDate = new Date(
+      new Date().getTime() + 60 * 60 * 24 * 7 * 10 * 1000
+    ).toISOString();
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://www.googleapis.com/calendar/v3/calendars/${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_EMAIL}/events?key=${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_API_KEY}
+          &singleEvents=true&orderBy=startTime&timeMin=${startDate}&timeMax=${endDate}`
+        );
+
+        const offset = new Date().getTimezoneOffset() * 60000;
+        const data = await response.json();
+        const items = data.items.map((item) => {
+          item.allDay = !item.start.dateTime;
+          (item.start = item.start.dateTime
+            ? new Date(item.start.dateTime)
+            : new Date(new Date(item.start.date).getTime() + offset)),
+            (item.end = new Date(
+              item.end.dateTime || new Date(item.end.date).getTime() + offset
+            )),
+            (item.hidden = false);
+          return item;
+        });
+        setEvents(items);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="relative flex flex-col">
@@ -39,7 +61,7 @@ const CalendarEvents = () => {
             onNavigate={(newDate) => {
               setDate(newDate);
             }}
-            events={dummyEvents}
+            events={events}
             localizer={mLocalizer}
             defaultView="month"
             views={["month"]}
@@ -70,6 +92,10 @@ const CalendarEvents = () => {
               };
             }}
           />
+          {selectedEvent && (
+            <Modal setEvents={setSelectedEvent} events={selectedEvent} />
+          )}
+          {console.log(selectedEvent)}
         </div>
       </section>
     </div>
