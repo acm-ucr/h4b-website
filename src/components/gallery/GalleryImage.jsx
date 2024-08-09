3020bd1870ac87cf701677c4803cb56bdcb0a477
Jefren Ofiaza <jefrenofiaza@gmail.com>
2024-08-08 18:54:07 -0700

    Jefre/QuarterPicAnimation

---
+++ src/components/gallery/GalleryImage.jsx
@@ -0,0 +1,21 @@
+"use client";
+import useView from "@/components/useView";
+import React from "react";
+import Image from "next/image";
+
+const GalleryImage = ({ src, alt }) => {
+  const [inView, ref] = useView();
+
+  return (
+    <div
+      ref={ref}
+      className={`${
+        inView && "animate-fade-down"
+      } transition-transform duration-100 ease-in-out`}
+    >
+      <Image src={src} alt={alt} />
+    </div>
+  );
+};
+
+export default GalleryImage;
