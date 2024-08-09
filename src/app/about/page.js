71962340022da906f119837cb43745cb287f1059
Russell Villanueva <russellv.git@gmail.com>
2024-08-09 16:19:44 -0700

    fixes

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,12 +1,17 @@
+"use client";
+import React from "react";
 import springPicture from "@/public/images/spring/spring24_9.webp";
-import { ABOUT } from "@/data/about/About";
+import { ABOUT } from "@/data/aboutPage";
 import Title from "@/components/Title";
 import Picture from "@/components/Picture";
 import Subtitle from "@/components/Subtitle";
+import useView from "@/components/useView";
 
 const Page = () => {
+  const [inView, ref] = useView();
+
   return (
-    <div className="bg-blobs1 bg-cover bg-biscuits-green-100 flex flex-col items-center gap-y-10 p-4 md:p-10">
+    <div className="bg-biscuits-green-100 flex flex-col items-center gap-y-10 p-4 md:p-10">
       <Title text="About Us" color="red" />
       <Picture
         classname="flex justify-center"
@@ -15,14 +20,34 @@ const Page = () => {
         image={springPicture}
         alt="Spring 24 Picture"
       />
-      {ABOUT.map((section, index) => (
-        <div key={index} className="mb-8 text-center">
-          <Subtitle color="text-biscuits-green-200" text={section.subtitle} />
-          <div className="md:text-2xl text-base font-inika md:w-2/3 w-10/12 mx-auto">
-            {section.paragraph}
+      {ABOUT.map((section, index) => {
+        // Base animation class for both Subtitle and paragraphs
+        const animationClass =
+          "animate-fade-up animate-once animate-duration-[1500ms] animate-ease-linear";
+
+        // Define delays to stagger animations
+        const subtitleDelay = "animate-delay-0";
+        const paragraphDelay = "animate-delay-500"; // Adjust this to your liking
+
+        return (
+          <div key={index} ref={ref} className="mb-8 text-center">
+            <Subtitle
+              color="text-biscuits-green-200"
+              text={section.subtitle}
+              animate={inView}
+              animationClass={animationClass}
+              delay={subtitleDelay}
+            />
+            <div
+              className={`md:text-2xl text-base font-inika md:w-2/3 w-10/12 mx-auto ${
+                inView ? `${animationClass} ${paragraphDelay}` : ""
+              }`}
+            >
+              {section.paragraph}
+            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
--- src/components/Subtitle.jsx
+++ src/components/Subtitle.jsx
@@ -1,7 +1,11 @@
-const Subtitle = ({ color, text }) => {
+import React from "react";
+
+const Subtitle = ({ color, text, animate, animationClass, delay }) => {
   return (
     <div
-      className={`my-2 font-bold font-shrikhand text-center text-4xl md:text-7xl ${color}`}
+      className={`my-2 font-bold font-shrikhand text-center text-4xl md:text-7xl ${color} ${
+        animate ? `${animationClass} animate-delay-${delay}` : ""
+      }`}
     >
       {text}
     </div>
