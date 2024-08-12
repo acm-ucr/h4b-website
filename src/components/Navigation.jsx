fecd3aa08b1328795144c60b297e6fdcdcc2114e
ACM UCR <fake@domain.com>
2024-08-12 20:00:46 +0000

    convert images to webp

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
-import logo from "@/public/images/H4BNavLogo.png";
+import logo from "@/public/images/H4BNavLogo.webp";
 import { IoMdMenu } from "react-icons/io";
 import { ITEMS } from "@/data/Navigation.js";
 
