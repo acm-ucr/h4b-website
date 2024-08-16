a625cadc41e6040ac64f70f79e9d7546fae72f1b
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2024-08-15 23:37:44 -0700

    more fixes haha

--- src/components/gallery/TablingEvents.jsx
+++ src/components/gallery/TablingEvents.jsx
@@ -9,13 +9,14 @@ import useView from "@/components/useView";
 const TablingEvents = () => {
   const [inView, ref] = useView();
   return (
-    <div
-      ref={ref}
-      className={`${inView && "animate-fade-right animate-once"} mb-14`}
-    >
+    <div>
       <Subtitle text="Food Tabling" color="text-biscuits-green-200" />
-
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 place-items-center">
+      <div
+        ref={ref}
+        className={`${
+          inView && "animate-fade-right animate-once"
+        } mb-14 pt-10 grid grid-cols-1 lg:grid-cols-3 gap-8 place-items-center`}
+      >
         <Image src={Tabling1} alt="Tabling" />
         <Image src={Tabling2} alt="Tabling" />
         <Image src={Tabling3} alt="Tabling" />
