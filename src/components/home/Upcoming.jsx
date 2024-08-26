3c3d0f172e2bb96ce2b65d27a93aa5edb417fcc2
Fuyuki Malahom <fuyuki325@gmail.com>
2024-08-25 23:34:27 -0700

    gcal integration events

--- src/components/home/Upcoming.jsx
+++ src/components/home/Upcoming.jsx
@@ -2,9 +2,76 @@
 import UpcomingCard from "./UpcomingCard";
 import Title from "../Title";
 import useView from "@/components/useView";
+import { useEffect, useState } from "react";
 
 const Upcoming = () => {
   const [inView, ref] = useView();
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const startDate = new Date(
+      new Date().getTime() - 60 * 60 * 24 * 7 * 10 * 1000
+    ).toISOString();
+    const endDate = new Date(
+      new Date().getTime() + 60 * 60 * 24 * 7 * 10 * 1000
+    ).toISOString();
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://www.googleapis.com/calendar/v3/calendars/${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_EMAIL}/events?key=${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_API_KEY}
+          &singleEvents=true&orderBy=startTime&timeMin=${startDate}&timeMax=${endDate}`
+        );
+
+        const offset = new Date().getTimezoneOffset() * 60000;
+        const data = await response.json();
+        const maxEvents = 4;
+        const today = new Date();
+
+        const items = data.items
+          .filter((item) => {
+            item.allDay = !item.start.dateTime;
+
+            item.start = item.start.dateTime
+              ? new Date(item.start.dateTime)
+              : new Date(new Date(item.start.date).getTime() + offset);
+
+            item.end = new Date(
+              item.end.dateTime || new Date(item.end.date).getTime() + offset
+            );
+
+            return item.start >= today;
+          })
+          .slice(0, maxEvents);
+
+        // Extract the month
+        const itemsMap = items.map((item) => {
+          const month = item.start.toLocaleString("default", {
+            month: "short",
+          });
+
+          // Extract the day (number)
+          const day = item.start.getDate();
+
+          // Extract the time in AM/PM format
+          const time = item.start.toLocaleString("en-US", {
+            hour: "numeric",
+            minute: "numeric",
+            hour12: true,
+          });
+          item.month = month;
+          item.day = day;
+          item.time = time;
+          return item;
+        });
+
+        setEvents(itemsMap);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div
       ref={ref}
@@ -18,10 +85,54 @@ const Upcoming = () => {
           inView && "animate-fade-left"
         } grid md:grid-cols-2 grid-cols-1 gap-4`}
       >
-        <UpcomingCard />
-        <UpcomingCard />
-        <UpcomingCard />
-        <UpcomingCard />
+        {events[0] ? (
+          <UpcomingCard
+            event={events[0].summary}
+            location={events[0].location}
+            description={events[0].description}
+            month={events[0].month}
+            date={events[0].date}
+            time={events[0].time}
+          />
+        ) : (
+          ""
+        )}
+        {events[1] ? (
+          <UpcomingCard
+            event={events[1].summary}
+            location={events[1].location}
+            description={events[1].description}
+            month={events[1].month}
+            date={events[1].date}
+            time={events[1].time}
+          />
+        ) : (
+          ""
+        )}
+        {events[2] ? (
+          <UpcomingCard
+            event={events[2].summary}
+            location={events[2].location}
+            description={events[2].description}
+            month={events[2].month}
+            date={events[2].date}
+            time={events[2].time}
+          />
+        ) : (
+          ""
+        )}
+        {events[3] ? (
+          <UpcomingCard
+            event={events[3].summary}
+            location={events[3].location}
+            description={events[3].description}
+            month={events[3].month}
+            date={events[3].date}
+            time={events[3].time}
+          />
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
--- src/components/home/UpcomingCard.jsx
+++ src/components/home/UpcomingCard.jsx
@@ -8,26 +8,28 @@ const UpcomingCard = ({
 }) => {
   return (
     <div>
-      <div className="flex flex-row rounded-3xl">
-        <div className="bg-biscuits-purple-100 items-center rounded-l-3xl justify-center text-white text-shadow shadow-biscuits-purple-200 py-3 w-1/3">
-          <div className="text-3xl md:text-5xl text-center font-omc">
+      <div className="flex flex-row rounded-3xl border-2 border-black">
+        <div className="bg-biscuits-purple-100 items-center rounded-l-3xl justify-center text-white text-shadow py-3 w-1/3 font-bold">
+          <div className="text-5xl md:text-6xl text-center font-inika">
             {month}
           </div>
-          <div className="text-5xl md:text-7xl text-center font-omc">
+          <div className="text-7xl md:text-6xl text-center font-inika">
             {date}
           </div>
-          <div className="text-2xl md:text-4xl text-center font-omc">
+          <div className="text-2xl md:text-3xl text-center font-inika">
             {time}
           </div>
         </div>
         <div className="flex flex-col bg-white w-3/4 rounded-r-3xl text-left justify-center pl-2 md:pl-3 lg:pl-5">
-          <div className="text-2xl pl-1 sm:3xl md:4xl lg:text-5xl font-omc">
+          <div className="text-3xl md:4xl pl-1 font-inika font-bold">
             {event}
           </div>
-          <div className="text-sm sm:text-base pl-1 md:lg lg:text-m font-omc">
+          <div className="text-sm sm:text-base pl-1 md:lg lg:text-m font-inika font-bold">
             {location}
           </div>
-          <div className="text-xs pl-1 lg:text-sm pr-2">{description}</div>
+          <div className="text-xs pl-1 lg:text-sm pr-2 font-inika">
+            {description}
+          </div>
         </div>
       </div>
     </div>
