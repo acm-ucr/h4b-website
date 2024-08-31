6549d953eea388ceeefc36a96cd6caeae898c5e6
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2024-08-31 01:45:59 -0700

    button hover animation fix

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,22 +1,14 @@
 import Link from "next/link";
-import useView from "./useView";
 
 const Button = ({ text, link }) => {
-  const [inView, ref] = useView();
   return (
-    <div ref={ref}>
-      <Link
-        href={link}
-        target="_blank"
-        className={`${
-          inView
-            ? `animate-fade-up animate-once animate-duration-300 animate-ease-linear`
-            : ""
-        } w-fit bg-biscuits-pinkish text-biscuits-textColor font-inika py-1.5 px-8 md:py-2 md:px-12 rounded-full font-bold text-xl md:text-3xl hover:opacity-75 duration-100`}
-      >
-        {text}
-      </Link>
-    </div>
+    <Link
+      href={link}
+      target="_blank"
+      className="w-fit bg-biscuits-pinkish text-biscuits-textColor font-inika py-1.5 px-8 md:py-2 md:px-12 rounded-full font-bold text-xl md:text-3xl hover:opacity-75 duration-100"
+    >
+      {text}
+    </Link>
   );
 };
 export default Button;
